b72237388d918985cac1241c37f5af12c4dbcd0d
hlhanhl <huilin.han@colorado.edu>
2020-02-23 10:49:19 -0700

    Update ngram.py

--- ngram.py
+++ ngram.py
@@ -8,20 +8,24 @@ import random
 
 class ngram:
     model = defaultdict(lambda: defaultdict(lambda: 0))
-    
+    cur_index=0
     def __init__(self, wordlist):
         self.wordlist=list(wordlist)
         self.listlength=len(wordlist)
         self.wordused=[0]*len(wordlist)
-        self.threshold=0.005
+        self.threshold=0.01
     
     def train(self):
         self.model = defaultdict(lambda: defaultdict(lambda: 0))
 
         for sentence in gutenberg.sents():
             for a, b, c in trigrams(sentence, pad_right=True, pad_left=True):
-                if not(a=='None' or b=='None'):
-                    self.model[(a,b)][c]+=1
+                if not(a=='None' or b=='None' or c=='None'):
+                    if(isinstance(a, str) and isinstance(b, str) and isinstance(c, str)):
+                        if not (a.isdigit() or b.isdigit() or c.isdigit()):
+                            self.model[(a,b)][c]+=1
+                    else:
+                        self.model[(a,b)][c]+=1
 
         for a_b in self.model:
             total=float(sum(self.model[a_b].values()))
@@ -41,7 +45,7 @@ class ngram:
         cur_bigram=[starting_words[0],starting_words[1]]
         sent=[starting_words[0],starting_words[1]]
         sent_length=0
-
+        self.wordlist.append('edhfjeifkgjrjdf')
         
         while(sentence_end==0):
             new_word=self.choose_word(self.wordlist[cur_word],cur_bigram)
@@ -49,19 +53,23 @@ class ngram:
                 cur_word+=1
                 if (cur_word == self.listlength):
                     words_done=1
-                else:
-                    print('Cur word is ', self.wordlist[cur_word])
+            if (new_word=='#END'):
+                sentence_end=1
+                new_word=' '
             if (words_done == 1):
-                if (new_word=='.' or new_word==';' or new_word=='!' or new_word=='?'):
+                if (new_word=='.' or new_word==';' or new_word=='!' or new_word=='?' or new_word=='#END'):
                     sentence_end=1
+                    if (new_word == ';' or new_word =='#END'):
+                        new_word='.'
             cur_bigram[0]=cur_bigram[1]
             cur_bigram[1]=new_word
-            sent.append(new_word)
-            sent_length+=1
+            if (new_word is not None):
+                sent.append(new_word)
+                sent_length+=1
             
-            if (sent_length>50):
+            if (sent_length>100):
                 sentence_end=1
-            
+            self.cur_index=cur_word
         print(sent)
         return sent
             
@@ -76,13 +84,25 @@ class ngram:
                         chosen_word=word
                         ourword_used=True
                         break
-                    bag.append(word)
+                    if isinstance(word,str):
+                        if not word.isdigit():
+                            bag.append(word)
         if (ourword_used)==False:
-            chosen_word=random.choice(bag)
-        print('Chosen word is',chosen_word)
+            if (len(bag)==0):
+                chosen_word='#END'
+            else:
+                chosen_word=random.choice(bag)
+        #print('Chosen word is',chosen_word)
         return chosen_word
             
     
-a=ngram(['she','think','this'])
+a=ngram(['Philippine','think','this'])
 a.train()
-a.generate()       
+c=0
+text=[]
+while(a.cur_index < a.listlength):
+    s=a.generate()
+    text.append(s)
+    if (c>3):
+        break
+    c+=1
